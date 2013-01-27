00b171ad6f31b591ce3a621fd490701edfe48aa0
atutor <info@atutor.ca>
2013-01-27 18:01:58 -0500

    replace default hello world text

--- module_delete.php
+++ module_delete.php
@@ -6,16 +6,11 @@
  * $course is the ID of the course to delete.
  */
 
-function course_seats($course) {
+function course_seats_delete($course) {
 	global $db;
-
 	// delete hello_world course table entries
-	$sql = "DELETE FROM ".TABLE_PREFIX."hello_world WHERE course_id=$course";
+	$sql = "DELETE FROM ".TABLE_PREFIX."course_seats WHERE course_id=$course";
 	mysql_query($sql, $db);
-
-	// delete hello_world course files
-	$path = AT_CONTENT_DIR .'hello_world/' . $course .'/';
-	clr_dir($path);
 }
 
 ?>
