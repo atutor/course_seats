319e6ddabb848ce80d8d8190213e5c04cebbef14
Greg <greg@atutorspaces.com>
2013-01-27 15:06:04 -0500

    adjust Create course path to accomodate service module

--- module.php
+++ module.php
@@ -20,22 +20,39 @@ define('AT_ADMIN_PRIV_COURSE_SEATS', $this->getAdminPrivilege());
  * add the admin pages when needed.
  */
 if (admin_authenticate(AT_ADMIN_PRIV_COURSE_SEATS, TRUE) || admin_authenticate(AT_ADMIN_PRIV_ADMIN, TRUE)) {
-global $_config;
+	global $_config;
 	// replace the create course tab for admin, removed by "disable_create" config setting
 	// remove the following lines if installed on atsp
 	if($_config['disable_create'] == "1"){
+	
+	$sql = "SELECT * FROM ".TABLE_PREFIX."modules WHERE dir_name ='_core/services' && status ='2'";
+	$result = mysql_query($sql, $db);
+	if(mysql_num_rows($result) != 0 ){
+	    //This is a Service site 
+		$service_site = 1;
+	}
+	}
+	if($service_site){
+		//$this->_pages['mods/_core/services/admin/create_course.php']['title_var'] = '';
+		//$this->_pages['mods/_core/services/admin/create_course.php']['parent']    = 'mods/_core/courses/admin/courses.php';
+		//$this->_pages['mods/_core/services/admin/create_course.php']['guide']     = 'admin/?p=creating_courses.php';	
+		//$this->_pages['mods/_core/courses/admin/courses.php']['children']  = array('mods/_core/services/admin/create_course.php');
+		$this->_pages['mods/_core/courses/admin/create_course.php']['title_var'] = '';
+	} else{
 		$this->_pages['mods/_core/courses/admin/create_course.php']['title_var'] = 'create_course';
 		$this->_pages['mods/_core/courses/admin/create_course.php']['parent']    = 'mods/_core/courses/admin/courses.php';
 		$this->_pages['mods/_core/courses/admin/create_course.php']['guide']     = 'admin/?p=creating_courses.php';	
-		$this->_pages['mods/_core/courses/admin/courses.php']['children']  = array('mods/_core/courses/admin/create_course.php');
-	}
-	//If {ayments is installed, add tab to payment manager
+	//	$this->_pages['mods/_core/courses/admin/courses.php']['children']  = array('mods/_core/services/admin/create_course.php');
+	} 
+	
+
+	//If Payments is installed, add tab to payment manager
 	if($_config['ec_uri']){
 		$this->_pages['mods/payments/payments_admin.php']['children'] = array_merge(array('mods/course_seats/index_admin.php'), isset($this->_pages['mods/payments/payments_admin.php']['children']) ? $this->_pages['mods/payments/payments_admin.php']['children'] : array());;
 	}
 	$this->_pages['mods/_core/courses/admin/courses.php']['children']  = array_merge(array('mods/course_seats/index_admin.php'), isset($this->_pages['mods/_core/courses/admin/courses.php']['children']) ? $this->_pages['mods/_core/courses/admin/courses.php']['children'] : array());
 	$this->_pages['mods/course_seats/index_admin.php']['title_var'] = 'seats_course_seats';
-	$this->_pages['mods/course_seats/index_admin.php']['parent']    = 'mods/_core/courses/admin/courses.php';
+	//$this->_pages['mods/course_seats/index_admin.php']['parent']    = 'mods/_core/courses/admin/courses.php';
 	
 	$this->_pages['mods/course_seats/seats_config.php']['title_var'] = 'seats_config';
 	$this->_pages['mods/course_seats/seats_config.php']['parent']    = 'mods/course_seats/index_admin.php';
